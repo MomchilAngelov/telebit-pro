ffb984a98140f182a2e25d0e6552246ee74bb571
MomchilAngelov <momodota2@gmail.com>
2016-11-22 21:02:01 +0200

    Adding more sutfff

--- webserver/WebServer.py
+++ webserver/WebServer.py
@@ -1,13 +1,18 @@
 import socket
 import sys
 import os
+import random
 import threading
+import subprocess
+from contextlib import redirect_stdout
+import io
 
 master_root = os.getcwd()
 master_root += "/"
 master_root += sys.argv[1]
 
 buff_size = 4096
+
 
 class ClientHandler(threading.Thread):
 
@@ -18,20 +23,8 @@ class ClientHandler(threading.Thread):
 		self.addr = addr
 
 	def run(self):
-		data_to_send = ""
-		data = self.socket.recv(buff_size)
-		print(data.decode("utf-8"))
-
-		if data:
-			print("нещо си")
-			data = self.socket.recv(buff_size)
-			print(data.decode("utf-8"))
-
-		print("хере")
-
-		data = data.decode("utf-8")
-		lines = data.split("\n")
-
+		lines = self.socket.recv(buff_size).decode("utf-8").split("\n")
+		
 		basic_stuff = lines[0]
 
 		method_type, url, http_version = basic_stuff.split(" ")
@@ -50,7 +43,6 @@ class ClientHandler(threading.Thread):
 		else:
 			file_path += url
 
-		# GET parameters handled! (put in another function)
 		get_parameters = {}
 		if has_get_params:
 
@@ -61,13 +53,58 @@ class ClientHandler(threading.Thread):
 
 		try:
 
-			with open(file_path, "rb") as f:
-				self.socket.sendfile(f)
+			with open(file_path, "r") as f:
+				tmp_file_path = master_root + "/tmp/{0}.html".format(random.uniform(1, 10))
+				
+				tmp_file = self.parse_file(f, tmp_file_path, get_parameters)
+
+				self.socket.sendfile(tmp_file)
+				tmp_file.close()
+				os.remove(tmp_file_path)
 
 		except FileNotFoundError as e:
-			with open(master_root+"404.html", "rb") as f:
+			print(e)
+			with open(master_root+"/404.html", "rb") as f:
 				self.socket.sendfile(f)			
 		self.socket.close()
+
+	def parse_file(self, file_hanler, tmp_file_path, data):
+		temp_file = open(tmp_file_path, "w")
+
+		python_code = """"""
+		found_python_code = 0
+		parse_python_code = 0
+		for line_number, line in enumerate(file_hanler, 1):
+			if "{{" in line:
+				found_python_code = 1
+				continue
+			if "}}" in line:
+				parse_python_code = 1
+
+			if parse_python_code:
+
+				f = io.StringIO()
+				with redirect_stdout(f):
+					exec(python_code, {}, locals())
+
+				for python_code_output_line in f.getvalue().split("\n"):
+					temp_file.write(python_code_output_line + "<br />")
+				parse_python_code = 0
+				found_python_code = 0
+				python_code = ""
+				continue
+
+			if found_python_code:
+				python_code += line
+				continue
+
+			temp_file.write(line)
+
+		temp_file.close()
+		temp_file = open(tmp_file_path, "rb")
+
+		return temp_file
+
 
 HOST = '127.0.0.1'
 PORT = 80
@@ -83,7 +120,7 @@ except OSError as e:
 	print(e)
 	sys.exit(1)
 	
-s.listen(100)
+s.listen(10)
 
 i = 1
 while 1:
@@ -94,6 +131,7 @@ while 1:
 		print("User number {0}".format(i))
 		clientHandler = ClientHandler(user_id = i, socket = connection_socket, addr = addr)
 		clientHandler.run()
+		print("Connection lost!")
 		i += 1
 
 	except KeyboardInterrupt as e:
