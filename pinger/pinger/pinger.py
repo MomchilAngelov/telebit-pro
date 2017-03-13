c73fb714ade5b9c762fe153d64b5718b383dde63
MomchilAngelov <momchil.angelov.1997@gmail.com>
2017-03-13 13:23:32 +0200

    Addding some files from the 2 tasks - security_bug_tracker and async pinger

--- pinger/pinger/pinger.py
+++ pinger/pinger/pinger.py
@@ -1,12 +1,18 @@
 import gevent
+from gevent import Timeout
 from gevent.subprocess import Popen, PIPE
 import re
+
+timeout_seconds = 1
+package_count = 5
 
 all_threads = []
 data = {}
 
 def parse_result(some_string):
 	"""
+	EXAMPLE:
+
 		PING 216.58.212.35 (216.58.212.35) 56(84) bytes of data.
 		64 bytes from 216.58.212.35: icmp_seq=1 ttl=57 time=21.7 ms
 		64 bytes from 216.58.212.35: icmp_seq=2 ttl=57 time=1.22 ms
@@ -21,26 +27,30 @@ def parse_result(some_string):
 	packet_loss = packet_loss_group.group(0)
 
 	rtt_group = re.search(r"(?<=mdev = )\d+\.\d+/\d+\.\d+/\d+\.\d+/\d+\.\d+", some_string)
-	
-	values_arr = rtt_group.group(0).split("/")
-	
+	try:
+		values_arr = rtt_group.group(0).split("/")
+	except Exception as e:
+		#Total loss of pings..
+		return 100, "N/A", "N/A" 
+
 	rtt_avg = float(values_arr[1])
 	mdev = float(values_arr[3])
 
 	return packet_loss, rtt_avg, mdev
 
-def cron(ip, number_of_packages, current_order):
-	sub = Popen(['ping -c {0} {1}'.format(number_of_packages, ip)], stdout=PIPE, shell=True)
+def ping(ip, number_of_packages, current_order, timeout):
+	#print('ping -i {0} -q -W {1} -c {2} {3}'.format(timeout/number_of_packages, timeout, number_of_packages, ip))
+	sub = Popen(['ping -i {0} -q -W {1} -c {2} {3}'.format(timeout/number_of_packages, timeout, number_of_packages, ip)], stdout=PIPE, shell=True)
 	out, err = sub.communicate()
 	data[current_order] = [ip, out]
 
+
 with open("test_Data/ips", "r") as f:
 	for current_order, current_ip in enumerate(f):
-		g = gevent.spawn(cron, ip = current_ip, number_of_packages = 2, current_order = current_order)
+		g = gevent.spawn(ping, ip = current_ip, number_of_packages = package_count, current_order = current_order, timeout = timeout_seconds)
 		all_threads.append(g)
 
-gevent.joinall(all_threads, timeout=10)
-
+gevent.joinall(all_threads)
 
 for key, value in data.items():
 	packet_loss, rtt_avg, mdev = parse_result(str(value[1], "utf-8"))
---
+++ security_bug_tracker/security_bug_tracker/get_data.py
@@ -0,0 +1,120 @@
+import urllib.request
+import platform, sys
+
+import argparse
+
+from bs4 import BeautifulSoup
+
+origin = "https://security-tracker.debian.org"
+site_with_packages = "https://security-tracker.debian.org/tracker/status/release/stable"
+
+def getOs():
+	print(platform.release())
+	print(platform.platform())
+	print(platform.system())
+	print(platform.version())
+	print(platform.dist())
+	print(platform.linux_distribution())
+
+	return "jessie"
+
+def getUrgency():
+	urgency = sys.argv[2]
+	return_urgency = set()
+	if "h" in urgency:
+		return_urgency.add("high")
+	if "m" in urgency:
+		return_urgency.add("medium")
+	if "u" in urgency:
+		return_urgency.add("not yet assigned")
+	if "l" in urgency:
+		return_urgency.add("low")
+
+	return return_urgency
+
+def getPackages():
+	return ["gst-plugins-bad1.0"]
+
+def openWebsiteWithBS(website):
+	return BeautifulSoup(urllib.request.urlopen(website).read(), "lxml")
+
+def urgency_level(row, urgency):
+	urg = row.find_all('td')[2].string.replace("*", "")
+	if urg in urgency:
+		return urg
+	else:
+		return None 
+
+def getSourcePackageAndItsChildren(website, package, table_part):
+	links = []
+	
+	for tr in website.find_all('table')[table_part].find_all('tr'):
+		if tr.find('td'):
+			if tr.find('td'):
+				links.append(tr.find('td'))
+
+	could_be_multiple = False
+
+	part_of_table = []
+	for link in links:
+		a = link.find('a')
+
+		if (a is not None) and ("/tracker/source-package/" + package in a["href"]):
+			could_be_multiple = True
+			part_of_table.append(a.parent.parent)
+
+		elif (a is None or a["href"] == "/tracker/source-package/") and could_be_multiple: 
+			part_of_table.append(link.parent)
+
+		elif could_be_multiple:
+			break
+
+	return part_of_table
+
+def getAllBugs(website, urgency_level_specified, packages):
+	dict_package_to_bugs = {}
+	for package in packages:
+		dict_package_to_bugs[package] = {}
+
+		mini_table = getSourcePackageAndItsChildren(website, package, 0)
+		for row in mini_table:
+			urgency_level_returned = urgency_level(row, urgency_level_specified)
+			if urgency_level_returned is not None:
+				bug_link = row.find_all('td')[1].find('a')['href']
+				dict_package_to_bugs[package][bug_link] = {}
+				dict_package_to_bugs[package][bug_link]["urgency"] = urgency_level_returned
+
+	return dict_package_to_bugs
+
+def getOsVulnerabillity(table, dist):
+	for row in table:
+		if row.find('td'):
+			columns = row.find_all('td')
+			if dist in (dist.strip() for dist in columns[1].string.split(",")):
+				if 'vulnerable' in columns[3].string.split(","):
+					return True
+				else:
+					return False
+
+
+os = getOs()
+urgency_level_specified = getUrgency()
+packages = getPackages()
+
+website = openWebsiteWithBS(site_with_packages)
+
+package_to_links = getAllBugs(website = website, urgency_level_specified = urgency_level_specified, packages = packages)
+
+for package_name, bug_hash in package_to_links.items():
+
+	for bug in bug_hash:
+		bug_hash[bug] = {}
+		print("Parsing data for bug", bug.split("/")[-1], "found in package", package_name)
+
+		website = openWebsiteWithBS(origin + bug)
+		part_of_table = getSourcePackageAndItsChildren(website, package_name, 1)
+
+		is_vulnerable = getOsVulnerabillity(part_of_table, os)
+		bug_hash[bug]["vulnerable"] = is_vulnerable
+		bug_hash[bug]["os"] = os
+		print("Your OS '{0}' is {1} to {2}".format(os, "vulnerable" if is_vulnerable else "not vulnerable", origin+bug))
