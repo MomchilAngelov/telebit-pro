ecafa8c19e85a05b233e16b3c34d8a59bb2abf61
MomchilAngelov <momchil.angelov.1997@gmail.com>
2017-03-17 10:39:27 +0200

    making some changes on the pinger, making it look cooler and stuff

--- pinger/pinger/pinger.py
+++ pinger/pinger/pinger.py
@@ -8,28 +8,95 @@ parser.add_argument("-c", "--configure", help="Give the ip json file", type = st
 parser.add_argument("-p", "--packet", help="The number of packets sent to the host/ip", type = int, default = False)
 parser.add_argument("-t", "--time", help="The time the packets should be bashed on the host!", type = int, default = False)
 parser.add_argument("-v", "--verbose", help="Give the script more verbosity, make it yell!", action = "store_true", default = False)
-
 args = parser.parse_args()
 
 DEBUG = args.verbose 
 
-all_threads = []
-data = {}
-host_to_ip = {}
-output_json = {}
-versionNumber = "3.0"
-applicationName = "Momo's little Pinger"
-defaultSearchFolder = "test_Data/ips"
+def main():
+	all_threads = []
+	data = {}
+	host_to_ip = {}
+	output_json = {}
+	versionNumber = "3.0"
+	applicationName = "Momo's little Pinger"
+	defaultSearchFolder = "test_Data/input.json"
 
-if not (args.time == 0 or args.packet == 0):
-	if args.time/args.packet < 0.2:
-		print("We can't do that...\nYou are going too fast!")
-		sys.exit()
+	if not (args.time == 0 or args.packet == 0):
+		if args.time/args.packet < 0.2:
+			print("We can't do that...\nYou are going too fast!")
+			sys.exit()
+
+	timeout_seconds, package_count = getInitialValues(args.time, args.packet)
+	host_to_ip = getDataFromFile(args.configure or defaultSearchFolder)
+
+	for current_order, current_ip in enumerate(host_to_ip):
+		if len(current_ip) == 3:
+			g = gevent.spawn(ping, ip = current_ip[0], number_of_packages = current_ip[1],
+				current_order = current_order, speed = current_ip[2], host = None, data = data)
+		else:
+			g = gevent.spawn(ping, ip = current_ip[0], number_of_packages = current_ip[1],
+				current_order = current_order, speed = current_ip[2], host = current_ip[3], data = data)
+		
+		all_threads.append(g)
+
+	gevent.joinall(all_threads)
+
+	output_json["version"] = versionNumber
+	output_json["applications"] = {}
+	output_json["applications"]["icmp_pings"] = {}
+	output_json["applications"]["icmp_pings"]["name"] = applicationName
+	output_json["applications"]["icmp_pings"]["items"] = {}
+	items = output_json["applications"]["icmp_pings"]["items"]
+
+	for key, value in data.items(): 
+		packet_loss, rtt_avg, mdev = parseResult(value[1].decode())
+		
+		items[value[0].strip()+".rtt"] = {
+			"value": rtt_avg, 
+			"units": "ms", 
+			"name": "[{0} -> {1}] ICMP ping: packet round trip time".format(value[0].strip(), value[3]), 
+			"type": "float", 
+			"timestamp": int(value[2]), 
+			"domain": value[3], 
+			"command": value[4]
+		}
+
+		items[value[0].strip()+".packet_loss"] = {
+			"value": packet_loss,
+			"units": "%", "name": "[{0} -> {1}] ICMP ping: packet loss".format(value[0].strip(), value[3]),
+			"type": "float",
+			"timestamp": int(value[2]), 
+			"domain": value[3],
+			"command": value[4]
+		}
+		
+		# items[value[0].strip()+".rtt"] = {}	
+		# items[value[0].strip()+".rtt"]["value"] = rtt_avg
+		# items[value[0].strip()+".rtt"]["units"] = "ms"
+		# items[value[0].strip()+".rtt"]["name"] = "[{0} -> {1}] ICMP ping: packet round trip time".format(value[0].strip(), value[3])
+		# items[value[0].strip()+".rtt"]["type"] = "float"
+		# items[value[0].strip()+".rtt"]["timestamp"] = int(value[2])
+		# items[value[0].strip()+".rtt"]["domain"] = value[3]
+		# items[value[0].strip()+".rtt"]["command"] = value[4]
+
+
+
+		# items[value[0].strip()+".packet_loss"] = {}
+		# items[value[0].strip()+".packet_loss"]["value"] = packet_loss
+		# items[value[0].strip()+".packet_loss"]["units"] = "%"
+		# items[value[0].strip()+".packet_loss"]["name"] = "[{0} -> {1}] ICMP ping: packet loss".format(value[0].strip(), value[3])
+		# items[value[0].strip()+".packet_loss"]["type"] = "float"
+		# items[value[0].strip()+".packet_loss"]["timestamp"] = int(value[2])
+		# items[value[0].strip()+".packet_loss"]["domain"] = value[3]
+		# items[value[0].strip()+".packet_loss"]["command"] = value[4]
+
+	output_json_final = json.dumps(output_json, ensure_ascii=False, indent = 4)
+	print(output_json_final)
+
 
 def isGoodIPv4(s):
 	pieces = s.split('.')
-	if len(pieces) != 4: return False
-	
+	if len(pieces) != 4: return False	
 	try: return all(0<=int(p)<256 for p in pieces)
 	except ValueError: return False
 
@@ -38,7 +105,6 @@ def normalizeInputForHostname(hostname):
 	hostname = hostname.replace("http://", "", 1)
 	if hostname[-1] == "/":
 		hostname = hostname[:-1]
-	
 	return hostname	
 
 def resolveDomainName(hostname):
@@ -58,8 +124,12 @@ def resolveDomainName(hostname):
 
 def getDataFromFile(file):
 	"""
-		TMP ARRAY STRUCTURE:
-			ip, number of packages, timeout, host (if any)
+		Input:
+			the relative path to a file
+
+		output:
+			array with structure:
+				ip, number of packages, timeout, host (if any)
 	"""
 
 	if DEBUG:
@@ -77,8 +147,8 @@ def getDataFromFile(file):
 				temporary_item.append(valueIdx["packet_interval"])
 				tmp_arr.append(temporary_item)
 			else:
-				ips = resolveDomainName(valueIdx["address"])
-				for ip in ips:
+				domain_to_ips = resolveDomainName(valueIdx["address"])
+				for ip in domain_to_ips:
 					temporary_item.append(ip)
 					temporary_item.append(valueIdx["packets_count"])
 					temporary_item.append(valueIdx["packet_interval"])
@@ -103,7 +173,7 @@ def getInitialValues(time, packet):
 
 	return time, packet
 
-def parse_result(some_string):
+def parseResult(some_string):
 
 	packet_loss_group = re.search(r"(?<=received, )[0-9]+", some_string)
 	packet_loss = packet_loss_group.group(0)
@@ -129,49 +199,4 @@ def ping(ip, number_of_packages, current_order, speed, host, data):
 	data[current_order] = [ip, out, time.time(), host, command]
 
 
-timeout_seconds, package_count = getInitialValues(args.time, args.packet)
-host_to_ip = getDataFromFile(args.configure or defaultSearchFolder)
-
-for current_order, current_ip in enumerate(host_to_ip):
-	if len(current_ip) == 3:
-		g = gevent.spawn(ping, ip = current_ip[0], number_of_packages = current_ip[1],
-			current_order = current_order, speed = current_ip[2], host = None, data = data)
-	else:
-		g = gevent.spawn(ping, ip = current_ip[0], number_of_packages = current_ip[1],
-			current_order = current_order, speed = current_ip[2], host = current_ip[3], data = data)
-	
-	all_threads.append(g)
-
-gevent.joinall(all_threads)
-
-output_json["version"] = versionNumber
-output_json["applications"] = {}
-output_json["applications"]["icmp_pings"] = {}
-output_json["applications"]["icmp_pings"]["name"] = applicationName
-output_json["applications"]["icmp_pings"]["items"] = {}
-items = output_json["applications"]["icmp_pings"]["items"]
-
-for key, value in data.items():
-	packet_loss, rtt_avg, mdev = parse_result(value[1].decode())
-	
-	items[value[0].strip()+".rtt"] = {}
-	items[value[0].strip()+".rtt"]["value"] = rtt_avg
-	items[value[0].strip()+".rtt"]["units"] = "ms"
-	items[value[0].strip()+".rtt"]["name"] = "[{0} -> {1}] ICMP ping: packet round trip time".format(value[0].strip(), value[3])
-	items[value[0].strip()+".rtt"]["type"] = "float"
-	items[value[0].strip()+".rtt"]["timestamp"] = int(value[2])
-	items[value[0].strip()+".rtt"]["domain"] = value[3]
-	items[value[0].strip()+".rtt"]["command"] = value[4]
-
-
-	items[value[0].strip()+".packet_loss"] = {}
-	items[value[0].strip()+".packet_loss"]["value"] = packet_loss
-	items[value[0].strip()+".packet_loss"]["units"] = "%"
-	items[value[0].strip()+".packet_loss"]["name"] = "[{0} -> {1}] ICMP ping: packet loss".format(value[0].strip(), value[3])
-	items[value[0].strip()+".packet_loss"]["type"] = "float"
-	items[value[0].strip()+".packet_loss"]["timestamp"] = int(value[2])
-	items[value[0].strip()+".packet_loss"]["domain"] = value[3]
-	items[value[0].strip()+".packet_loss"]["command"] = value[4]
-
-output_json_final = json.dumps(output_json, ensure_ascii=False, indent = 4)
-print(output_json_final)
+main()
