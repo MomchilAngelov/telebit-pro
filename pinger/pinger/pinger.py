3ccb90a4284d445f31d2ac21cb0530ee0a47441d
MomchilAngelov <momchil.angelov.1997@gmail.com>
2017-03-19 14:02:56 +0200

    Upgrading the output format

--- pinger/pinger/pinger.py
+++ pinger/pinger/pinger.py
@@ -31,7 +31,7 @@ def main():
 
 	all_threads = [gevent.spawn(ping, ip = current_data[0], number_of_packages = current_data[1], 
 		current_order = current_order, speed = current_data[2], 
-		host = current_data[3], data = data, requesting_application = current_data[4])
+		host = current_data[3], data = data, requesting_application = current_data[4], requesting_application_name = current_data[5])
 		for current_order, current_data in enumerate(host_to_data)	
 	]
 
@@ -39,56 +39,70 @@ def main():
 
 	output_json["version"] = versionNumber
 	output_json["applications"] = {}
-	output_json["applications"]["icmp_pings"] = {}
-	output_json["applications"]["icmp_pings"]["name"] = applicationName
-	output_json["applications"]["icmp_pings"]["items"] = {}
-	items = output_json["applications"]["icmp_pings"]["items"]
 
-	for key, value in data.items(): 
-		packet_loss, rtt_avg, mdev = parseResult(value[1].decode())
+	#if old output format should be reversed
+	#all in one big items hash, instead of segregated per application that requested it 
+	# output_json["applications"]["icmp_pings"] = {}
+	# output_json["applications"]["icmp_pings"]["name"] = applicationName
+	# output_json["applications"]["icmp_pings"]["items"] = {}
+	# items = output_json["applications"]["icmp_pings"]["items"]
+
+	previous_value = None
+	for key, list_with_command_result in data.items():
+		if not list_with_command_result[5] == previous_value:
+			previous_value = list_with_command_result[5]
+			output_json["applications"][list_with_command_result[5]] = {}
+			output_json["applications"][list_with_command_result[5]]["name"] = list_with_command_result[6]
+			output_json["applications"][list_with_command_result[5]]["items"] = {}
+			
 		
-		items[value[0].strip()+".rtt"] = {
+		items = output_json["applications"][list_with_command_result[5]]["items"]
+
+		packet_loss, rtt_avg, mdev = parseResult(list_with_command_result[1].decode())
+		
+		items[list_with_command_result[0].strip()+".rtt"] = {
 			"value": rtt_avg, 
 			"units": "ms", 
-			"name": "[{0} -> {1}] ICMP ping: packet round trip time".format(value[0].strip(), value[3]), 
+			"name": "[{0} -> {1}] ICMP ping: packet round trip time".format(list_with_command_result[0].strip(), list_with_command_result[3]), 
 			"type": "float", 
-			"timestamp": int(value[2]), 
-			"domain": value[3], 
-			"command": value[4],
-			"requesting_application": value[5]
+			"timestamp": int(list_with_command_result[2]), 
+			"domain": list_with_command_result[3], 
+			"command": list_with_command_result[4],
+			"requesting_application": list_with_command_result[5]
 		}
 
-		items[value[0].strip()+".packet_loss"] = {
+		items[list_with_command_result[0].strip()+".packet_loss"] = {
 			"value": packet_loss,
-			"units": "%", "name": "[{0} -> {1}] ICMP ping: packet loss".format(value[0].strip(), value[3]),
+			"units": "%", "name": "[{0} -> {1}] ICMP ping: packet loss".format(list_with_command_result[0].strip(), list_with_command_result[3]),
 			"type": "float",
-			"timestamp": int(value[2]), 
-			"domain": value[3],
-			"command": value[4],
-			"requesting_application": value[5]
+			"timestamp": int(list_with_command_result[2]), 
+			"domain": list_with_command_result[3],
+			"command": list_with_command_result[4],
+			"requesting_application": list_with_command_result[5]
 		}
 		
-		# items[value[0].strip()+".rtt"] = {}	
-		# items[value[0].strip()+".rtt"]["value"] = rtt_avg
-		# items[value[0].strip()+".rtt"]["units"] = "ms"
-		# items[value[0].strip()+".rtt"]["name"] = "[{0} -> {1}] ICMP ping: packet round trip time".format(value[0].strip(), value[3])
-		# items[value[0].strip()+".rtt"]["type"] = "float"
-		# items[value[0].strip()+".rtt"]["timestamp"] = int(value[2])
-		# items[value[0].strip()+".rtt"]["domain"] = value[3]
-		# items[value[0].strip()+".rtt"]["command"] = value[4]
+		# items[list_with_command_result[0].strip()+".rtt"] = {}	
+		# items[list_with_command_result[0].strip()+".rtt"]["value"] = rtt_avg
+		# items[list_with_command_result[0].strip()+".rtt"]["units"] = "ms"
+		# items[list_with_command_result[0].strip()+".rtt"]["name"] = "[{0} -> {1}] ICMP ping: packet round trip time".format(list_with_command_result[0].strip(), list_with_command_result[3])
+		# items[list_with_command_result[0].strip()+".rtt"]["type"] = "float"
+		# items[list_with_command_result[0].strip()+".rtt"]["timestamp"] = int(list_with_command_result[2])
+		# items[list_with_command_result[0].strip()+".rtt"]["domain"] = list_with_command_result[3]
+		# items[list_with_command_result[0].strip()+".rtt"]["command"] = list_with_command_result[4]
 
 
 
-		# items[value[0].strip()+".packet_loss"] = {}
-		# items[value[0].strip()+".packet_loss"]["value"] = packet_loss
-		# items[value[0].strip()+".packet_loss"]["units"] = "%"
-		# items[value[0].strip()+".packet_loss"]["name"] = "[{0} -> {1}] ICMP ping: packet loss".format(value[0].strip(), value[3])
-		# items[value[0].strip()+".packet_loss"]["type"] = "float"
-		# items[value[0].strip()+".packet_loss"]["timestamp"] = int(value[2])
-		# items[value[0].strip()+".packet_loss"]["domain"] = value[3]
-		# items[value[0].strip()+".packet_loss"]["command"] = value[4]
+		# items[list_with_command_result[0].strip()+".packet_loss"] = {}
+		# items[list_with_command_result[0].strip()+".packet_loss"]["value"] = packet_loss
+		# items[list_with_command_result[0].strip()+".packet_loss"]["units"] = "%"
+		# items[list_with_command_result[0].strip()+".packet_loss"]["name"] = "[{0} -> {1}] ICMP ping: packet loss".format(list_with_command_result[0].strip(), list_with_command_result[3])
+		# items[list_with_command_result[0].strip()+".packet_loss"]["type"] = "float"
+		# items[list_with_command_result[0].strip()+".packet_loss"]["timestamp"] = int(list_with_command_result[2])
+		# items[list_with_command_result[0].strip()+".packet_loss"]["domain"] = list_with_command_result[3]
+		# items[list_with_command_result[0].strip()+".packet_loss"]["command"] = list_with_command_result[4]
 
 	output_json_final = json.dumps(output_json, ensure_ascii=False, indent = 4)
+	
 	print(output_json_final)
 
 
@@ -180,6 +194,7 @@ def getDataFromFile(file):
 				temporary_item.append(valueIdx["packet_interval"])
 				temporary_item.append(None)
 				temporary_item.append(application_name)
+				temporary_item.append(input_json["applications"][application_name]["name"])
 				tmp_arr.append(temporary_item)
 			else:
 				domain_to_ips = resolveDomainName(valueIdx["address"])
@@ -189,6 +204,7 @@ def getDataFromFile(file):
 					temporary_item.append(valueIdx["packet_interval"])
 					temporary_item.append(valueIdx["address"])
 					temporary_item.append(application_name)
+					temporary_item.append(input_json["applications"][application_name]["name"])
 					tmp_arr.append(temporary_item)
 					temporary_item = []
 	
@@ -225,14 +241,14 @@ def parseResult(some_string):
 
 	return packet_loss, rtt_avg, mdev
 
-def ping(ip, number_of_packages, current_order, speed, host, data, requesting_application):
+def ping(ip, number_of_packages, current_order, speed, host, data, requesting_application, requesting_application_name):
 	command = 'ping -i {0} -q -W {1} -c {2} {3}'.format(speed, speed * number_of_packages, number_of_packages, ip)
 	if DEBUG:
 		print(command)
 
 	sub = Popen([command], stdout=PIPE, shell=True)
 	out, err = sub.communicate()
-	data[current_order] = [ip, out, time.time(), host, command, requesting_application]
+	data[current_order] = [ip, out, time.time(), host, command, requesting_application, requesting_application_name]
 
 
 main()
