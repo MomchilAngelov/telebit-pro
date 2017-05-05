87a7c301461f9b907f08c551ec0c8f6f2f2dcf0d
momo <momchil.angelov.1997@gmail.com>
2017-05-05 20:46:18 +0300

    Making some cleanup of the working directory

--- pinger/pinger/testing_expansion.py
+++
@@ -1,7 +0,0 @@
-import fnmatch
-import os
-import glob
-
-my_glob_result = glob.glob("test_Data/input.json")
-for python_file in my_glob_result:
-	print(python_file)
--- pinger/pinger/testing_hook_with_module_import.py
+++
@@ -1,3 +0,0 @@
-from error_handling_library import test_hooks
-
-print(2/0)
