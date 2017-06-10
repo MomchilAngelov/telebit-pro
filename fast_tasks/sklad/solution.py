cb7175f983e694678e763f1673275abd3ae01d37
MomchilAngelov <momchil.angelov.1997@gmail.com>
2017-06-10 09:10:42 +0300

    Adding the sklad fast task ;)

---
+++ fast_tasks/sklad/solution.py
@@ -0,0 +1,58 @@
+matrix = []
+length = input()
+
+for i in range(int(length)):
+	row = input()
+	matrix.append([int(x) for x in row.split()])
+
+def findRoutes(matrix):
+	routes = 0
+	for row in matrix:
+		if sum(row) == 0:
+			routes += 1
+
+	for i in range(len(matrix)):
+		currentSum = 0
+		for row in matrix:
+			currentSum += row[i]
+
+		if currentSum == 0:
+			routes += 1
+
+	return routes
+
+def findDifferentGoods(matrix):
+	numberOfGoods = 0
+	prevFlagIndex = []
+	FlagIndex = []
+
+	for row in matrix:
+		subArr = []
+		for idx, value in enumerate(row):
+			if value > 0:
+				subArr.append(idx)
+			if value == 0 and subArr:
+				FlagIndex.append(subArr[:])
+				subArr = []
+		if subArr:
+			FlagIndex.append(subArr[:])
+			subArr = []
+
+
+
+		for arr in prevFlagIndex:
+			if arr not in FlagIndex:
+				numberOfGoods += 1
+
+		prevFlagIndex = FlagIndex
+		FlagIndex = []
+
+	numberOfGoods += len(prevFlagIndex)
+
+	return numberOfGoods
+
+for row in matrix:
+	print(row)
+
+#print(findRoutes(matrix))
+print(findDifferentGoods(matrix))
