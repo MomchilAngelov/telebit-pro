f98596df7f830f4157d4935b970909602863b257
momo <momchil.angelov.1997@gmail.com>
2017-06-21 18:04:53 +0300

    Adding latest stuff

--- sourcesListParser/parser.py
+++ sourcesListParser/parser.py
@@ -7,11 +7,34 @@
 	suites: oldstable, stable, testing, unstable, or experimental
 """
 
-import json
+import json, re, sys
 from urllib.request import urlopen
 
+godRepoToLevel = {
+	'normal': 'https://deb.debian.org/debian',
+	'debug': 'https://deb.debian.org/debian-debug',
+	'ports': 'https://deb.debian.org/debian-ports',
+	'security': 'https://deb.debian.org/debian-security',
+}
+
 def getSourceFileFromLink(link):
-	print('Release file: ', link + '/Release')
+	print(link)
+	fileURL = link + '/Release'
+	file = str(urlopen(fileURL).read(), 'utf-8').split('\n')
+	return file
+
+def getTypeOfRepoLink(distro):
+	if 'backports' in distro:
+		return godRepoToLevel['ports']
+	elif '/updates' in distro:
+		return godRepoToLevel['security']
+	else:
+		return godRepoToLevel['normal']
+
+def getDateFromFile(file):
+	for line in file:
+		if line.startswith('Date'):
+			return line
 
 my_ip = str(urlopen('http://ip.42.pl/raw').read(), 'utf-8')
 
@@ -37,24 +60,23 @@ with open('/etc/apt/sources.list') as f:
 		# print(js['country_code'])
 		# print(country.upper() == js['country_code'])
 
-		if country.upper() == js['country_code']:
-			print('The country is fine')
-		else:
-			print('The mirror is bad!')
+		# if country.upper() == js['country_code']:
+		# 	print('The country is fine')
+		# else:
+		# 	print('The mirror is bad!')
 
-		print('Type: {0}, uri: {1}, distribution: {2}, packages: {3}'.format( ('binary package' if data[0] == 'deb' else 'source package'), data[1], data[2], [x for x in data[3:-1]] ))
+		#print('Type: {0}, uri: {1}, distribution: {2}, packages: {3}'.format( ('binary package' if data[0] == 'deb' else 'source package'), data[1], data[2], [x for x in data[3:-1]] ))
 		
-		links = 'Package link: {0}'.format(data[1] + '/dists/' + data[2])		
-		
-		for component in data[3:-1]:
-			source = getSourceFileFromLink('{0}/{1}'.format(data[1] + '/dists/' + data[2], component))
+		links = 'Package link: {0}'.format(data[1] + '/dists/' + data[2])
+		godBaseLink = getTypeOfRepoLink(data[2])
 
+		components = data[3:-1]
 
-		if data[3:-1] == []:
-			print(data)
-			source = getSourceFileFromLink('{0}/{1}'.format(data[1] + '/dists', data[2]))
+		source = getSourceFileFromLink('{0}'.format(data[1] + '/dists/' + data[2]))
+		sourceTestAgainst = getSourceFileFromLink('{0}/dists/{1}'.format(godBaseLink, data[2]))
 
-
-		print()
+		dateSource = getDateFromFile(source)
+		dateSourceTestAgainst = getDateFromFile(sourceTestAgainst)
+		print(dateSource == dateSourceTestAgainst)
 
 
